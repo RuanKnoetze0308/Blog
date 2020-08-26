9478186d3d75f77bde37b2365be00c98704b43ae
RuanKnoetze0308 <ruan.knoetze15@gmail.com>
2020-08-26 11:38:52 +0200

    Add project files.

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Data;
+using Blog.Data.FileManager;
+using Blog.Data.Repository;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repo;
+        private IFileManager _fileManager;
+        public HomeController(
+            IRepository repo,
+            IFileManager fileManager)
+        {
+            _repo = repo;
+            _fileManager = fileManager;
+        }
+        public IActionResult Index(string Category)
+        {
+            
+            var posts = string.IsNullOrEmpty(Category)? _repo.GetAllPosts() : _repo.GetAllPosts(Category);
+            //boolean? true : false; 1=1? run: ignore;
+            return View(posts);
+        }
+        public IActionResult Post(int id)
+        {
+            var post = _repo.GetPost(id);
+
+            return View(post);
+        }
+        [HttpGet("/Image/{image}")]
+        public IActionResult Image(string image)
+        {
+            var mime = image.Substring(image.LastIndexOf('.') + 1); 
+            return new FileStreamResult(_fileManager.ImageStream(image), $"image/{mime}");
+        }
+    }
+}
---
+++ Blog/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Data;
+using Blog.Data.FileManager;
+using Blog.Data.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => 
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            
+            })
+                //.AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+            });
+
+            services.AddTransient<IRepository, Repository>();
+            services.AddTransient<IFileManager, FileManager>();
+
+            services.AddMvc();
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+               
+            }
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
+
+           
+        }
+    }
+}
